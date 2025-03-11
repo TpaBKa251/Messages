1feb3e333c37a0262dd523d7e0fa315fed31a10a
darmren <dem24@tpu.ru>
2025-03-12 01:43:44 +0700

    HOSTEL-35 Миграционный скрипт для создания таблиц

---
+++ src/main/resources/db/migration/V1.1__create_tables.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "broadcasts" (
+    "id" UUID NOT NULL UNIQUE,
+    "theme" TEXT NOT NULL,
+    "created_at" TIMESTAMP,
+    PRIMARY KEY("id")
+);
+
+
+CREATE TABLE messages_meta (
+    "id" UUID NOT NULL UNIQUE,
+    "title" TEXT,
+    "broadcast_id" UUID NOT NULL REFERENCES broadcasts(id),
+    "sender" UUID NOT NULL REFERENCES "user".users ("id"),
+    "scheduled_at" TIMESTAMP NOT NULL,
+    PRIMARY KEY("id")
+);
+
+
+CREATE TABLE "subscriptions" (
+    "user_id" UUID NOT NULL REFERENCES "user".users("id"),
+    "broadcast_id" UUID NOT NULL REFERENCES broadcasts(id),
+    "started_at" TIMESTAMP NOT NULL,
+    PRIMARY KEY("user_id", "broadcast_id")
+);
+
+
+CREATE TABLE "message_contents" (
+    "id" UUID NOT NULL UNIQUE,
+    "message_id" UUID NOT NULL REFERENCES messages_meta("id"),
+    "message_part" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    PRIMARY KEY("id")
+);
+
+
+CREATE TABLE "message_images" (
+    "id" UUID NOT NULL UNIQUE,
+    "message_id" UUID NOT NULL REFERENCES messages_meta("id"),
+    "image_url" TEXT NOT NULL,
+    PRIMARY KEY("id")
+);
